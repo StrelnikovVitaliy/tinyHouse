1f7019f7de0b3a3631548a10909e50976485c487
strelnikov <strelnikov93vitaliy@gmail.com>
2020-05-02 12:22:19 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+
+const app = express();
+const port = 9000;
+const one: number = 1;
+const two: number = 2;
+const three: boolean = true;
+
+
+app.get('/', (_req, res) => res.send(`${one}+${two}=${one + two}`));
+app.listen(port);
+
+console.log('app run')
